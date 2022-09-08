b835d99a1c5a09ef556a84abee6b0a302aee0799
bbo-git <73382903+bbo-git@users.noreply.github.com>
2022-09-08 09:11:51 +0200

    create project

---
+++ case-Egenic-Jairo/case-Egenic-Jairo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  case-Egenic-Jairo
+//
+//  Created by Jairo on 08/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
